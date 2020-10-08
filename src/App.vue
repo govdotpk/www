f98d43503d18262033e7436fb7233b0ca224f40b
Shaji <schajee@gmail.com>
2020-10-08 11:19:24 +0500

    Initial commit with Vue

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <main>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-info">
+      <a class="navbar-brand" href="/">GOV.PK</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbar">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item"><router-link class="nav-link" :to="{ name: 'home' }">Home</router-link></li>
+          <li class="nav-item"><router-link class="nav-link" :to="{ name: 'about' }">About</router-link></li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" name="q" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-light my-2 my-sm-0" type="submit">Go</button>
+        </form>
+      </div>
+    </nav>
+    
+    <nav>
+      <ol class="breadcrumb rounded-0">
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item"><a href="#">Library</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Data</li>
+      </ol>
+    </nav>
+
+    <section class="container-fluid">
+      <router-view />
+    </section>
+    <footer class="py-3 bg-light">
+      <ul class="nav">
+        <li class="nav-item"><router-link class="nav-link" :to="{ name: 'help' }">Help</router-link></li>
+        <li class="nav-item"><router-link class="nav-link" :to="{ name: 'terms' }">Terms</router-link></li>
+        <li class="nav-item"><router-link class="nav-link" :to="{ name: 'privacy' }">Privacy</router-link></li>
+        <li class="nav-item"><router-link class="nav-link" :to="{ name: 'cookies' }">Cookies</router-link></li>
+        <li class="nav-item"><router-link class="nav-link" :to="{ name: 'accessibility' }">Accessibility</router-link></li>
+        <li class="nav-item"><router-link class="nav-link" :to="{ name: 'contact' }">Contact</router-link></li>
+      </ul>
+    </footer>
+  </main>
+</template>
+
+<style>
+  html, body { height: 100%; }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container-fluid">
+    
+    <div class="row">
+      <div class="col">
+        <h2>Federal Ministries / Departments</h2>
+        <ul>
+          <li><router-link :to="{ name: 'president' }">President of Pakistan</router-link></li>
+          <li>Prime Minister Office</li>
+          <li>National Assembly</li>
+          <li>Senate of Pakistan</li>
+          <li>Supreme Court</li>
+        </ul>
+        <ul>
+          <li>Cabinet Secretariat</li>
+          <li>Establishment Division</li>
+          <li>Ministry of Climate Change</li>
+          <li>Ministry of Commerce and Textile</li>
+          <li>Ministry of Communications</li>
+          <li>Ministry of Defence</li>
+          <li>Ministry of Defence Production</li>
+          <li>Ministry of Energy</li>
+          <li>Ministry of Federal Education and Professional Training</li>
+          <li>Ministry of Finance, Revenue, and Economic Affairs</li>
+          <li>Ministry of Foreign Affairs</li>
+          <li>Ministry of Housing and Works</li>
+          <li>Ministry of Human Rights</li>
+          <li>Ministry of Industries and Production</li>
+          <li>Ministry of Information, Broadcasting, National History and Literary Heritage</li>
+          <li>Ministry of Information Technology and Telecommunication</li>
+          <li>Ministry of Interior</li>
+          <li>Ministry of Inter-Provincial Coordination</li>
+          <li>Ministry of Kashmir Affairs and Gilgit-Baltistan</li>
+          <li>Ministry of Law and Justice</li>
+          <li>Ministry of Narcotics Control</li>
+          <li>Ministry of National Food Security and Research</li>
+          <li>Ministry of National Health Services, Regulations and Coordination</li>
+          <li>Ministry of Overseas Pakistanis and Human Resource Development</li>
+          <li>Ministry of Parliamentary Affairs</li>
+          <li>Ministry of Planning, Development and Reform</li>
+          <li>Ministry of Maritime Affairs</li>
+          <li>Ministry of Postal Services</li>
+          <li>Ministry of Privatization</li>
+          <li>Ministry of Railways</li>
+          <li>Ministry of Religious Affairs and Inter-faith Harmony</li>
+          <li>Ministry of Science and Technology</li>
+          <li>Ministry of States and Frontier Regions</li>
+          <li>Ministry of Statistics</li>
+          <li>Ministry of Water Resources</li>
+        </ul>
+      </div>
+      <div class="col">
+        <h2>Provincial Governments</h2>
+        <ul>
+          <li>Government of Balochistan</li>
+          <li>Government of Khyber Pakhtunkhwa</li>
+          <li>Government of Punjab</li>
+          <li>Government of Sindh</li>
+          <li>Government of Gilgit-Baltistan</li>
+          <li>Government of Azad Jammu and Kashmir</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
